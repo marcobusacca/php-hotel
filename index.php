eaa00102c636cc374f2228be4509acc996735225
marcobusacca <marcobusacca01@gmail.com>
2023-07-04 13:03:10 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!-- SCRIPT PHP -->
+<?php 
+
+?>
+
+<!-- TEMPLATE HTML -->
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Head Title -->
+        <title>php-hotel</title>
+    </head>
+    <body>
+    </body>
+</html>
